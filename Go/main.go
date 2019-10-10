020d9908a1e76b6246466248c517c39fc806f902
Jesus Lizama <dev@hellojeb.us>
2019-10-09 22:15:49 -0700

    update main

--- Go/main.go
+++ Go/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import(
+	_ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"fmt"
+	"log"
+)
 
-func main(){
-	fmt.Println("Testing main.go")
+func main() {
+	cnn, err := sql.Open("mysql", "docker:docker@tcp(db:3306)/test_db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	id := 1
+	var name string
+
+	if err := cnn.QueryRow("SELECT name FROM test_tb WHERE id = ? LIMIT 1", id).Scan(&name); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(id, name)
 }
